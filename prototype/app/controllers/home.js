977f66fbd38af94839f3e0b6ca86a9e24a36cf1b
longclee <long.c.lee@gmail.com>
2016-03-06 15:12:06 -0600

    newsletter

--- prototype/app/controllers/home.js
+++ prototype/app/controllers/home.js
@@ -3,7 +3,9 @@ var express = require('express'),
   mongoose = require('mongoose'),
   User = mongoose.model('User'),
   Group = mongoose.model('Group'),
-  Post = mongoose.model('Post');
+  Post = mongoose.model('Post'),
+  nodemailer = require('nodemailer'),
+  mailer = require('nodemailer-express-handlebars');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -275,3 +277,118 @@ function checkNew(group){
     checkNew(group);
   }
 }
+
+router.get('/mail/send', function (req,res){
+  
+  /*var smtpConfig = {
+      host: 'smtp.gmail.com', // hostname 
+      secure: true, // use SSL 
+      port: 465, // port for secure SMTP 
+      transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts 
+      auth: {
+        user: 'long.c.lee@gmail.com',
+        pass: 'L@#c@#l15_05'
+      }
+  };
+
+  var poolConfig = {
+      pool: true,
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+          user: 'long.c.lee@gmail.com',
+          pass: 'L@#c@#l15_05'
+      }
+  };
+
+  var directConfig = {
+      name: 'kommunity.com' // must be the same that can be reverse resolved by DNS for your IP
+  };
+  */
+  
+  var nodemailer = require('nodemailer');
+
+  // Create a SMTP transporter object
+  var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: 'long.c.lee@gmail.com',
+          pass: 'L@#c@#l15_05'
+      },
+      logger: true, // log to console
+      debug: true // include SMTP traffic in the logs
+  }, {
+      // default message fields
+
+      // sender info
+      from: 'kommunity <long.c.lee@gmail.com>',
+      headers: {
+          'X-Laziness-level': 1000 // just an example header, no need to use this
+      }
+  });
+
+  console.log('SMTP Configured');
+
+  // Message object
+  var message = {
+
+      // Comma separated list of recipients
+      to: '"Long" <chenglongli2015@u.northwestern.edu>',
+
+      // Subject of the message
+      subject: 'Nodemailer is unicode friendly ✔', //
+
+      // plaintext body
+      text: 'Hello to myself!',
+
+      // HTML body
+      html: '<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>' +
+          '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>',
+
+      // Apple Watch specific HTML body
+      watchHtml: '<b>Hello</b> to myself',
+
+      // An array of attachments
+      attachments: [
+
+          // String attachment
+          {
+              filename: 'notes.txt',
+              content: 'Some notes about this e-mail',
+              contentType: 'text/plain' // optional, would be detected from the filename
+          },
+
+          // Binary Buffer attachment
+          {
+              filename: 'image.png',
+              content: new Buffer('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
+                  '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
+                  'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC', 'base64'),
+
+              cid: 'note@example.com' // should be as unique as possible
+          },
+
+          /*
+          // File Stream attachment
+          {
+              filename: 'nyan cat ✔.gif',
+              path: __dirname + '/assets/nyan.gif',
+              cid: 'long.c.lee@gmail.com' // should be as unique as possible
+          }
+          */
+      ]
+  };
+
+  console.log('Sending Mail');
+  transporter.sendMail(message, function (error, info) {
+      if (error) {
+          console.log('Error occurred');
+          console.log(error.message);
+          res.redirect('..');
+      }
+      console.log('Message sent successfully!');
+      console.log('Server responded with "%s"', info.response);
+      res.redirect('..');
+  });
+});
